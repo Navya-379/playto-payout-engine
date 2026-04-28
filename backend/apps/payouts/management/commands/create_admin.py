6762946380bf18050ca12c97b44828f3899546ba
Navya Sree <navya221204@gmail.com>
2026-04-28 18:40:11 +0530

    Update create_admin.py

--- backend/apps/payouts/management/commands/create_admin.py
+++ backend/apps/payouts/management/commands/create_admin.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         User.objects.create_superuser(
             username="admn",
             email="admn@12345.com",
-            password="mypassword"
+            password="admn123"
         )
 
         print("Admin reset done")
