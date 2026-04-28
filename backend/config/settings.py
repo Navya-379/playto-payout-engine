8ee92e4ffc5769fc24a4a98a14b679928d067ebc
Navya Sree <navya221204@gmail.com>
2026-04-28 15:24:34 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,6 +99,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # ========================
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ========================
 # CELERY CONFIG
