ec466513f29aa799023d0cd6991d82441b19aa63
Navya Sree <navya221204@gmail.com>
2026-04-28 15:14:16 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -68,13 +68,22 @@ ASGI_APPLICATION = "backend.asgi.application"
 # ========================
 # DATABASE (Render/Postgres safe)
 # ========================
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+if os.getenv("DATABASE_URL"):
+    import dj_database_url
 
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # ========================
 # INTERNATIONALIZATION
 # ========================
