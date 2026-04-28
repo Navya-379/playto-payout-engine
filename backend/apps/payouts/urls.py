ce2adf76f8795336e0b93b223be79f118e11bad5
Navya Sree <navya221204@gmail.com>
2026-04-28 12:43:34 +0530

    Update urls.py

--- backend/apps/payouts/urls.py
+++ backend/apps/payouts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from django.http import JsonResponse
 
 from .views import MerchantDashboardView, PayoutCreateView
 
+def api_home(request):
+    return JsonResponse({
+        "message": "API v1 working"
+    })
+
 urlpatterns = [
+    path("", api_home),
     path("payouts", PayoutCreateView.as_view(), name="payout-create"),
     path("merchants/<int:merchant_id>/dashboard", MerchantDashboardView.as_view(), name="merchant-dashboard"),
 ]
