5a715e027ee681e4440afec6c72eb98081330dc4
Navya Sree <navya221204@gmail.com>
2026-04-28 12:35:46 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,12 @@
 from django.urls import include, path
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Playto Payout Engine API is running"
+    })
 
 urlpatterns = [
+    path("", home),
     path("api/v1/", include("apps.payouts.urls")),
 ]
